752873f757bf0e3083c95912dd5bce26fa1df06b
M.H SHAGOR <srq001100@gmail.com>
2026-03-05 04:07:08 +0600

    update

--- src/Commands/PublishAllCommand.php
+++ src/Commands/PublishAllCommand.php
@@ -7,6 +7,9 @@ use Illuminate\Support\Facades\Artisan;
 
 class PublishAllCommand extends Command
 {
+    private const TAG_COMPONENTS = 'components';
+    private const TAG_FILE_PICKER = 'file-picker';
+
     protected $signature = 'mhshagor:publish-all {--force : Overwrite any existing files}';
 
     protected $description = 'Publish mhshagor components and file-picker assets.';
@@ -15,21 +18,32 @@ class PublishAllCommand extends Command
     {
         $force = (bool) $this->option('force');
 
-        $this->info('Publishing [components] assets.');
-        $componentsExit = Artisan::call('vendor:publish', [
-            '--tag' => 'components',
+        $results = [
+            self::TAG_COMPONENTS => $this->publishTag(self::TAG_COMPONENTS, $force),
+            self::TAG_FILE_PICKER => $this->publishTag(self::TAG_FILE_PICKER, $force),
+        ];
+
+        foreach ($results as $tag => $exitCode) {
+            if ($exitCode !== self::SUCCESS) {
+                $this->error("Publishing [{$tag}] failed.");
+            }
+        }
+
+        return in_array(self::FAILURE, $results, true) ? self::FAILURE : self::SUCCESS;
+    }
+
+    private function publishTag(string $tag, bool $force): int
+    {
+        $this->info("Publishing [{$tag}] assets.");
+
+        $exitCode = Artisan::call('vendor:publish', [
+            '--tag' => $tag,
             '--force' => $force,
         ]);
-        $this->output->write(Artisan::output());
 
+        $this->output->write(Artisan::output());
         $this->newLine();
-        $this->info('Publishing [file-picker] assets.');
-        $filePickerExit = Artisan::call('vendor:publish', [
-            '--tag' => 'file-picker',
-            '--force' => $force,
-        ]);
-        $this->output->write(Artisan::output());
 
-        return ($componentsExit === 0 && $filePickerExit === 0) ? self::SUCCESS : self::FAILURE;
+        return $exitCode === 0 ? self::SUCCESS : self::FAILURE;
     }
 }
